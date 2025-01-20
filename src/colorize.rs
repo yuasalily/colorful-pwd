ff4ba9c4d8b5df9a60f41b63951f316e25b1efe7
yuasalily <yuasalily1011@gmail.com>
2025-01-20 14:06:38 +0000

    背景色追加

--- src/colorize.rs
+++ src/colorize.rs
@@ -9,13 +9,13 @@ pub fn colorize(color: bool, path: String) -> String {
         .chars()
         .enumerate()
         .map(|(i, c)| match i % 7 {
-            0 => c.to_string().red().to_string(),
-            1 => c.to_string().bright_red().to_string(),
-            2 => c.to_string().yellow().to_string(),
-            3 => c.to_string().green().to_string(),
-            4 => c.to_string().bright_blue().to_string(),
-            5 => c.to_string().blue().to_string(),
-            6 => c.to_string().purple().to_string(),
+            0 => c.to_string().red().on_bright_green().to_string(),
+            1 => c.to_string().bright_red().on_green().to_string(),
+            2 => c.to_string().yellow().on_blue().to_string(),
+            3 => c.to_string().green().on_purple().to_string(),
+            4 => c.to_string().bright_blue().on_bright_red().to_string(),
+            5 => c.to_string().blue().on_yellow().to_string(),
+            6 => c.to_string().purple().on_green().to_string(),
             _ => c.to_string().black().to_string(),
         })
         .collect::<String>();
